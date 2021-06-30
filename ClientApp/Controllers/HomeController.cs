e8bd515dbf40a215b813a31c145ae8bc32016465
Amol Sawkar <Amol.Sawkar@citiustech.com>
2021-06-30 16:10:16 +0530

    Add project files.

---
+++ ClientApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync("http://servhost/api/Test");
+
+            ViewBag.Response = result;
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
